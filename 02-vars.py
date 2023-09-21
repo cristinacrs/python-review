d51cde5859950d635cab4a7e8838ecc78a3687dc
Cristina Cruz Cortes <cristinacruzcortes@gmail.com>
2023-09-21 17:57:48 -0600

    variables

---
+++ 02-vars.py
@@ -0,0 +1,17 @@
+print("Hola vars")
+
+#variable
+my_name = "Cristina"
+
+print("Hola" + " " + my_name)
+
+#variable numero
+my_age = 25
+
+print(my_age)
+
+print("Hola", my_name, "tienes", my_age)
+
+my_name = input("¿cuál es tu nombre?")
+
+print("tu nombre es", my_name)
