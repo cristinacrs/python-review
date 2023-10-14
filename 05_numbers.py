1bcb81e69cdc33b8676f78e129d4cf78c1bb2ff5
Cristina Cruz Cortes <cristinacruzcortes@gmail.com>
2023-10-14 15:30:35 -0600

    numbers

---
+++ 05_numbers.py
@@ -0,0 +1,26 @@
+lives = 3
+print(type(lives))
+
+age = 12
+budget = 100
+height = 1.65
+
+temperature = 12.4
+print(type(temperature))
+
+lives = 2
+print(lives)
+lives = 1
+
+print(lives)
+lives = lives - 1
+print(lives - 1)
+lives -=3
+print(lives)
+
+number = 46000000000000000.3
+print(number)
+
+number = 0.00000000001
+print(number)
+
