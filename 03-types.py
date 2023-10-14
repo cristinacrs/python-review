a1d2af8717ad8fec00869d3f45d7c14e29495eec
Cristina Cruz Cortes <cristinacruzcortes@gmail.com>
2023-10-14 14:29:09 -0600

    data types

---
+++ 03-types.py
@@ -0,0 +1,25 @@
+#string
+
+my_name = "Cris"
+my_name = 'Lana'
+
+print('my name =>', my_name)
+
+print(type(my_name))
+
+#int
+
+my_age = 12
+print(my_age)
+
+#boolean
+
+is_single = True
+print('is_single', is_single)
+print(type(is_single))
+
+#inputs
+
+my_age = int(input('whats your age?'))
+print(my_age)
+print(type(my_age))
