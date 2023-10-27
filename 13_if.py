89099ed88c226e21ae34f133e6bc0b2541a3ee17
Cristina Cruz Cortes <cristinacruzcortes@gmail.com>
2023-10-26 20:52:11 -0600

    conditionals and rock, scissors vs paper

---
+++ 13_if.py
@@ -0,0 +1,29 @@
+if True:
+    print('should run')
+
+if False:
+    print('never runs')
+
+
+
+pet = input('What is your favourite pet? ')
+
+if pet == 'dog':
+    print('me too!')
+
+if pet == 'cat':
+    print('good luck')
+
+if pet == 'fish':
+    print('i used to have one')
+
+
+'''
+stock = int(input('What is the stock? '))
+
+if stock >= 100 and stock <= 100:
+    print('the stock its ok')
+else:
+    print('wrong number in stock')
+'''
+
--- main.py
+++ main.py
@@ -1,8 +1,23 @@
-print("Hola Freya")
+user_option = input('Rock, scissors or paper? ')
+computer_option = 'rock'
 
-print("Hello world :P")
+if user_option == computer_option:
+    print('Tie!')
+elif user_option == 'rock':
+    if computer_option == 'scissors':
+        print('You have won!')
+    else:
+        print('Paper wins vs rock!')
+elif user_option == 'paper':
+    if computer_option == 'rock':
+        print('paper wins vs rock')
+        print('You have won!')
+    else:
+        print('scissors wins vs paper!')
+elif user_option == 'scissors':
+    if computer_option == 'paper':
+        print('Scissors wins vs paper')
+        print('You have won!')
+    else:
+        print('Rock wins vs scissors!')
 
-print(12+5)
-print(2*3)
-print(12-5)
-print(8/2)
