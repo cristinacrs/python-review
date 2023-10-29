5f916595cfa8eb1fa12ff367330334f70ad72787
Cristina Cruz Cortes <cristinacruzcortes@gmail.com>
2023-10-28 19:49:23 -0600

    tuples

---
+++ 18_tuples.py
@@ -0,0 +1,27 @@
+numbers = (1,2,3,5)
+
+strings = ('nico', 'zule', 'santi')
+
+print(numbers)
+print('0 =>', numbers[0])
+print('-1 =>', numbers[-1])
+print(type(numbers))
+
+print(strings)
+print(type(strings))
+
+print(strings)
+print(strings.index('zule'))
+
+print(strings.index('zule'))
+print(strings.count('nico'))
+
+my_list = list(strings)
+
+print(type(my_list))
+
+my_list[1] = 'Cris'
+print(my_list)
+
+my_tuple = tuple(my_list)
+print(my_tuple)
