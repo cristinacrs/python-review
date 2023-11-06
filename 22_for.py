edd5569ea286adfee6fc8b727792551c8133afbd
Cristina Cruz Cortes <cristinacruzcortes@gmail.com>
2023-11-05 20:24:01 -0600

    loop for positive numbers

--- 22_for.py
+++ 22_for.py
@@ -43,3 +43,12 @@ people = [
 
 for person in people:
     print('name: ', person['name'])
+
+my_list = [1, -1, 2, -2, 3, -3, 4, -4]
+new_list = []
+
+for i in my_list:
+  if i > 0:
+    new_list.append(i)
+
+print(new_list)
