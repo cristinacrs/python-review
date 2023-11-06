cd4955f4852f56ec1affd52de509942792164163
Cristina Cruz Cortes <cristinacruzcortes@gmail.com>
2023-11-05 20:20:31 -0600

    loops

---
+++ 21_while.py
@@ -0,0 +1,26 @@
+
+'''
+while True:
+    print('done')
+
+counter = 0
+
+while counter < 10:
+    counter += 1
+    print(counter)
+
+counter = 0
+
+while counter < 20:
+    counter += 3
+    if counter == 15:
+        break
+    print(counter)
+'''
+counter = 0
+
+while counter < 20:
+    counter += 1
+    if counter < 15:
+        continue
+    print(counter)
---
+++ 22_for.py
@@ -0,0 +1,45 @@
+
+'''
+for element in range(1, 21):
+    print(element)
+'''
+
+my_list = [23, 45, 67, 89, 1]
+
+for i in my_list:
+    print(i)
+
+my_tuple = ('lana', 'freya', 'keisi')
+
+for i in my_tuple:
+    print(i)
+
+product = {
+    'name' : 'shirt',
+    'price' : 3.55,
+    'stock' : 89
+}
+
+for key in product:
+    print(key, '=>',product[key])
+
+for key, value in product.items():
+    print(key, '=>', value)
+
+people = [
+    {
+        'name': 'freya',
+        'age': 4
+    },
+    {
+        'name': 'lana',
+        'age': 9
+    },
+    {
+        'name': 'keisi',
+        'age': 6
+    },
+]
+
+for person in people:
+    print('name: ', person['name'])
